47019d0597ebdfb8f290067bdb79b2db6228e323
Roman Masliak <maslyak3@gmail.com>
2024-09-29 11:42:42 +0300

    feat: added search feature

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ search.addEventListener('click', () => {
 })
 search.addEventListener('keydown', (event) => {
     if (event.key === 'Enter')
-    fetchApi();
+    fetchApi(search.value);
 });
 
-async function fetchApi() {
+async function fetchApi(query) {
     const response = await fetch(
-      'https://api.unsplash.com/search/photos?page=1&query=office&client_id=Mdwg280ski4Qkel6nW_V02v9dOROUAtxaGqCdoslQs8'
+      `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=Mdwg280ski4Qkel6nW_V02v9dOROUAtxaGqCdoslQs8`
     );
     let result = await response.json();
-    console.log(result)
-    if (result.length > 0) {getPhotos(result) }
+        if (result.results.length > 0) {
+            getPhotos(result);
+    }
 
 }
 fetchApi()
@@ -23,20 +24,25 @@ let grid = document.querySelector('.grid');
 
 
 function getPhotos(result) {
-
         grid.innerHTML = "";
-        result.foreach(picture => {
-            const image = document.createElement('img');
-            image.src = picture.urls.small
-            image.classlist.add('img')
+    result.results.forEach(picture => {
+
+
+        const image = document.createElement('img');
+
+            image.src = picture.urls.small;
+            image.classList.add('img');
+            image.width = 50;
             grid.append(image);
 
         })
-
-
 }
 
 
-// img.innerHTML = response.results.urls.small_s3;
-
-// https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=984ae24ab42310467bfb640a790d1eaa&text=winter&format=rest&auth_token=72157720930077804-12ddde883df57621&api_sig=4bcf7507b100b5f08853c325517e44de
+let button = document.querySelector('.submit')
+button.addEventListener('click', () => {
+  fetchApi(search.value)
+})
+window.onload = function () {
+  search.focus();
+};
